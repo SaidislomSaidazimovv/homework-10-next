e3b6d5b5cf646303246b335593cacf2fefdbaa6c
SaidislomSaidazimovv <saidazimovsaidislom97@gmail.com>
2024-10-09 08:49:24 +0500

    first commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+import SideBar from '@/components/sideBar/SideBar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex'>
+        <SideBar />
+    </div>
+  )
+}
+
+export default page
---
+++ components/sideBar/SideBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className='w-[300px] bg-green-500 h-screen'>SideBar</div>
+  )
+}
+
+export default SideBar
